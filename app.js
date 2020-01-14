efac567b16fe1e064bcd38bb254b90b270f2d541
alejovillegas7 <alejovillegaslopez@gmail.com>
2020-01-14 16:45:40 -0500

    connecting to a local mongodb DB called confection_machines_store

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/confection_machines_store", { useUnifiedTopology: true, useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
